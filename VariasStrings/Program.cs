bc58d8363934d104b5b522dd1b2952eb3add71a0
Maggiee <telesmaegarida@gmail.com>
2023-03-02 12:21:56 +0000

    Diferentes Strings

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cumprimentar = "Olá!";
+            string teste = @"C:\Users\teles\Documents";
+            string Unicode = "que venha o ver\u00e3o";
+            string escape = "Um tab\t";
+
+            Console.WriteLine(cumprimentar+", "+teste+" "+Unicode+"\n"+escape);
+        }
+    }
+}
