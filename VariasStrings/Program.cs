cc6822685078b1715ea69298dbce2e95cf08c59a
Maggiee <telesmaegarida@gmail.com>
2023-03-02 12:32:02 +0000

    Variaveis string por concatenação, interpolação, string.format()

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -12,6 +12,14 @@ namespace VariasStrings
             string escape = "Um tab\t";
 
             Console.WriteLine(cumprimentar+", "+teste+" "+Unicode+"\n"+escape);
+
+            string y = "34";
+            string x = "a" + 2;
+            Console.WriteLine("abc" + x);
+            Console.WriteLine($"valor de x é {x}");
+            Console.WriteLine( $"{x} mais {y} é igual a {x+y}");
+            Console.WriteLine(String.Format("Y é {1}, X é {0}", x, y));
+            Console.WriteLine(@"Verbatim com x={0}", x);
         }
     }
 }
