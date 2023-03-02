2d61ae6d9d6e51eb4949c8257963eea3cc7c5b80
Maggiee <telesmaegarida@gmail.com>
2023-03-02 12:05:24 +0000

    Adicionei variaveis e imprimi no ecrã

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 100;
+            char letra = 'a';
+            double numeros = 234.78;
+            bool afirmacao = true;
+            Console.WriteLine(numero1);
+            Console.WriteLine(letra);
+            Console.WriteLine(numeros);
+            Console.WriteLine(afirmacao);
+
+        }
+    }
+}
