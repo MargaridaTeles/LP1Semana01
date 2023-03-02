789f4b438068bbb731c68dc09166df88e57140ea
Maggiee <telesmaegarida@gmail.com>
2023-03-02 11:52:16 +0000

    Ciclo for

---
+++ Argumentos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i > args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+        }
+    }
+}
