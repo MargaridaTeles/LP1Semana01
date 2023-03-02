9f2ed221893c84f7426b2a8b8112462e1d2b6059
Maggiee <telesmaegarida@gmail.com>
2023-03-02 18:56:27 +0000

    strings com formatações (casas decimais, hexadecimal, moeda e percentagem)

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -6,6 +6,11 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
+            double xx = 0.12345;
+            string xx2 = $"xx={xx:f2}";
+            string decix = String.Format("{0:p1}", xx);
+            int ii = 18;
+
             string cumprimentar = "Olá!";
             string teste = @"C:\Users\teles\Documents";
             string Unicode = "que venha o ver\u00e3o";
@@ -20,6 +25,12 @@ namespace VariasStrings
             Console.WriteLine( $"{x} mais {y} é igual a {x+y}");
             Console.WriteLine(String.Format("Y é {1}, X é {0}", x, y));
             Console.WriteLine(@"Verbatim com x={0}", x);
+
+
+            Console.WriteLine(xx2);
+            Console.WriteLine(decix);
+            Console.WriteLine(ii.ToString("X"));
+            Console.WriteLine(ii.ToString("C"));
         }
     }
 }
