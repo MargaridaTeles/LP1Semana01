a87099183d21cdaf924423b0ab882103513618c7
Maggiee <telesmaegarida@gmail.com>
2023-03-02 12:11:39 +0000

    Adicionei variaveis reais de diferentes tipos

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -9,11 +9,13 @@ namespace TiposVarios
             int numero1 = 100;
             char letra = 'a';
             double numeros = 234.78;
+            float numeross = 43.245f;
             bool afirmacao = true;
             Console.WriteLine(numero1);
             Console.WriteLine(letra);
             Console.WriteLine(numeros);
             Console.WriteLine(afirmacao);
+            Console.WriteLine(numeross);
 
             char coracao = '♡';
             char aviao = '✈';
